4a5f6be57531b24bc81c768c45543e05d107a70a
Zayne <zayne@ideaslab.com>
2026-03-18 09:28:39 -0700

    Case sensitive app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import random
 import os
 
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__, template_folder="Templates")
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Load article metadata
