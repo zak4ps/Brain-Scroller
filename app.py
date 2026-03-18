8b1e96d300e9d25014329e4d4f0e2dcb6625eeb5
Zayne <zayne@ideaslab.com>
2026-03-18 09:27:34 -0700

    Changed directories in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import random
 import os
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Load article metadata
