b61a9ad7757756143e3c4ed41ea6755af8728edc
Zayne Kratz <zaynekratz@gmail.com>
2026-03-19 22:03:30 -0700

    fixed port

--- app.py
+++ app.py
@@ -133,4 +133,4 @@ def index():
     )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
